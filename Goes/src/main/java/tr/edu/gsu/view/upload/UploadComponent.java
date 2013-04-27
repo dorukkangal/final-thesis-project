d2e8f0c7d773c17645725a934924986f782f30ef
dorukkangal <doruk.kangal@imona.com.tr>
2013-04-27 15:51:46 +0300

    bug fix

--- Goes/src/main/java/tr/edu/gsu/view/upload/UploadComponent.java
+++ Goes/src/main/java/tr/edu/gsu/view/upload/UploadComponent.java
@@ -42,17 +42,19 @@ import com.vaadin.ui.Window;
 import com.vaadin.ui.Window.Notification;
 import com.vaadin.ui.themes.Reindeer;
 
+@SuppressWarnings("serial")
 public class UploadComponent extends VerticalLayout implements ClickListener, DropHandler, Receiver, StartedListener, FinishedListener, SucceededListener,
 		FailedListener, ProgressListener {
-	private static final long serialVersionUID = 1L;
 
+	private VerticalLayout startLayout;
 	private Upload upload;
 	private Label orLabel;
-	private Label waitLabel;
-	private Panel dropPanel;
+	private DragAndDropWrapper dropFileComponent;
 
-	private HorizontalLayout progressLayout;
+	private VerticalLayout uploadLayout;
+	private Label pleaseWaitLabel;
 	private Label descriptionLabel;
+	private HorizontalLayout progressLayout;
 	private ProgressIndicator progressIndicator;
 	private Button cancelButton;
 
@@ -65,23 +67,39 @@ public class UploadComponent extends VerticalLayout implements ClickListener, Dr
 
 	public UploadComponent(List<String> acceptedMimeTypes) {
 		this.acceptedMimeTypes = acceptedMimeTypes;
-
-		addUpload();
-		addOrLabel();
-		addDropPanel();
-		addPleaseWaitLabel();
-		addProgressLayout();
-	}
-
-	@Override
-	public void attach() {
-		super.attach();
-		startedMode();
+		init();
+		startMode();
 		setSizeFull();
 	}
 
-	// UI initialisation
-	private void addUpload() {
+	private void init() {
+		startLayout = buildStartLayout();
+		addComponent(startLayout);
+
+		uploadLayout = buildUploadLayout();
+		addComponent(uploadLayout);
+	}
+
+	private VerticalLayout buildStartLayout() {
+		startLayout = new VerticalLayout();
+		startLayout.setSizeFull();
+
+		upload = buildUpload();
+		startLayout.addComponent(upload);
+		startLayout.setComponentAlignment(upload, Alignment.MIDDLE_CENTER);
+
+		orLabel = buildOrLabel();
+		startLayout.addComponent(orLabel);
+		startLayout.setComponentAlignment(orLabel, Alignment.MIDDLE_CENTER);
+
+		dropFileComponent = buildDropFileComponent();
+		startLayout.addComponent(dropFileComponent);
+		startLayout.setComponentAlignment(dropFileComponent, Alignment.MIDDLE_CENTER);
+
+		return startLayout;
+	}
+
+	private Upload buildUpload() {
 		upload = new Upload();
 		upload.setButtonCaption(Messages.getValue(GoesConstants.MESSAGE_SELECT));
 		upload.setImmediate(true);
@@ -93,66 +111,87 @@ public class UploadComponent extends VerticalLayout implements ClickListener, Dr
 		upload.addListener((Upload.FinishedListener) this);
 		upload.addListener((Upload.SucceededListener) this);
 
-		addComponent(upload);
-		setComponentAlignment(upload, Alignment.MIDDLE_CENTER);
+		return upload;
 	}
 
-	protected void addOrLabel() {
+	protected Label buildOrLabel() {
 		orLabel = new Label(Messages.getValue(GoesConstants.MESSAGE_OR));
 		orLabel.setSizeUndefined();
 		orLabel.addStyleName(Reindeer.LABEL_SMALL);
-		addComponent(orLabel);
-		setComponentAlignment(orLabel, Alignment.MIDDLE_CENTER);
+
+		return orLabel;
 	}
 
-	protected void addDropPanel() {
-		dropPanel = new Panel();
-		DragAndDropWrapper dragAndDropWrapper = new DragAndDropWrapper(dropPanel);
-		dragAndDropWrapper.setDropHandler(this);
-		dragAndDropWrapper.setWidth("80%");
-		addComponent(dragAndDropWrapper);
-		setComponentAlignment(dragAndDropWrapper, Alignment.MIDDLE_CENTER);
+	protected DragAndDropWrapper buildDropFileComponent() {
+		Panel dropPanel = new Panel();
 
 		Label dropLabel = new Label(Messages.getValue(GoesConstants.MESSAGE_DROP_FILE));
 		dropLabel.setSizeUndefined();
 		dropPanel.addComponent(dropLabel);
 		((VerticalLayout) dropPanel.getContent()).setComponentAlignment(dropLabel, Alignment.MIDDLE_CENTER);
+
+		dropFileComponent = new DragAndDropWrapper(dropPanel);
+		dropFileComponent.setDropHandler(this);
+		dropFileComponent.setWidth("80%");
+
+		return dropFileComponent;
 	}
 
-	private void addPleaseWaitLabel() {
-		waitLabel = new Label(Messages.getValue(GoesConstants.MESSAGE_WAIT));
-		waitLabel.setVisible(false);
-		addComponent(waitLabel);
+	private VerticalLayout buildUploadLayout() {
+		uploadLayout = new VerticalLayout();
+		uploadLayout.setSizeFull();
+
+		pleaseWaitLabel = buildPleaseWaitLabel();
+		uploadLayout.addComponent(pleaseWaitLabel);
+		uploadLayout.setComponentAlignment(pleaseWaitLabel, Alignment.MIDDLE_CENTER);
+
+		descriptionLabel = buildDescriptionLabel();
+		uploadLayout.addComponent(descriptionLabel);
+		uploadLayout.setComponentAlignment(descriptionLabel, Alignment.MIDDLE_CENTER);
+
+		progressLayout = buildProgressLayout();
+		uploadLayout.addComponent(progressLayout);
+		uploadLayout.setComponentAlignment(progressLayout, Alignment.MIDDLE_CENTER);
+
+		return uploadLayout;
 	}
 
-	private void addProgressLayout() {
+	private Label buildPleaseWaitLabel() {
+		pleaseWaitLabel = new Label(Messages.getValue(GoesConstants.MESSAGE_WAIT));
+		pleaseWaitLabel.setVisible(false);
+		return pleaseWaitLabel;
+	}
+
+	private Label buildDescriptionLabel() {
+		descriptionLabel = new Label();
+		descriptionLabel.addStyleName(Reindeer.LABEL_SMALL);
+		return descriptionLabel;
+	}
+
+	private HorizontalLayout buildProgressLayout() {
 		progressLayout = new HorizontalLayout();
 		progressLayout.setSpacing(true);
 		progressLayout.setSizeUndefined();
-		addComponent(progressLayout);
 
-		addDescription();
-		addProgressIndicator();
-		addCancelButton();
-	}
-
-	private void addDescription() {
-		descriptionLabel = new Label();
-		descriptionLabel.addStyleName(Reindeer.LABEL_SMALL);
-		addComponent(descriptionLabel);
-	}
-
-	private void addProgressIndicator() {
-		progressIndicator = new ProgressIndicator();
+		progressIndicator = buildProgressIndicator();
 		progressLayout.addComponent(progressIndicator);
-		progressLayout.setComponentAlignment(progressIndicator, Alignment.MIDDLE_LEFT);
+
+		cancelButton = buildCancelButton();
+		progressLayout.addComponent(cancelButton);
+
+		return progressLayout;
 	}
 
-	private void addCancelButton() {
+	private ProgressIndicator buildProgressIndicator() {
+		progressIndicator = new ProgressIndicator();
+		return progressIndicator;
+	}
+
+	private Button buildCancelButton() {
 		cancelButton = new Button(GoesConstants.BUTTON_CANCEL);
 		cancelButton.addListener((ClickListener) this);
 		cancelButton.setStyleName(Reindeer.BUTTON_SMALL);
-		progressLayout.addComponent(cancelButton);
+		return cancelButton;
 	}
 
 	@Override
@@ -183,18 +222,17 @@ public class UploadComponent extends VerticalLayout implements ClickListener, Dr
 		if (!acceptedMimeTypes.contains(mimeType) || (event.getFilename() == null)) {
 			upload.interruptUpload();
 			AppContext.getMainWindow().showNotification("Error", Messages.getValue(GoesConstants.MESSAGE_FILE_MISMATCH), Notification.TYPE_ERROR_MESSAGE);
-			waitLabel.setVisible(false);
 			return;
 		}
 		descriptionLabel.setValue(event.getFilename());
 		progressIndicator.setValue(0f);
-		progressIndicator.setPollingInterval(500);
+		progressIndicator.setPollingInterval(50);
 		uploadMode();
 	}
 
 	@Override
 	public void uploadFinished(FinishedEvent event) {
-		startedMode();
+		startMode();
 	}
 
 	@Override
@@ -206,8 +244,10 @@ public class UploadComponent extends VerticalLayout implements ClickListener, Dr
 
 	@Override
 	public void uploadFailed(Upload.FailedEvent event) {
-		AppContext.getMainWindow().showNotification(Messages.getValue(GoesConstants.MESSAGE_INTERRUPT, event.getFilename()), Window.Notification.TYPE_WARNING_MESSAGE);
-		startedMode();
+		if (event != null)
+			AppContext.getMainWindow()
+					.showNotification(Messages.getValue(GoesConstants.MESSAGE_INTERRUPT, event.getFilename()), Window.Notification.TYPE_WARNING_MESSAGE);
+		startMode();
 		for (FailedListener failedListener : failedListeners) {
 			failedListener.uploadFailed(event);
 		}
@@ -221,42 +261,46 @@ public class UploadComponent extends VerticalLayout implements ClickListener, Dr
 	// Drag and drop handling (DropHandler)
 	@Override
 	public void drop(DragAndDropEvent event) {
-		waitLabel.setVisible(true);
-
 		WrapperTransferable transferable = (WrapperTransferable) event.getTransferable();
 		Html5File[] files = transferable.getFiles();
 		if (files == null)
 			return;
-		if (files.length > 0) {
+		if (files.length == 1) {
 			// only support for one file upload at this moment
 			final Html5File file = files[0];
 			file.setStreamVariable(new StreamVariable() {
-				private static final long serialVersionUID = 1L;
-
+				@Override
 				public void streamingStarted(StreamingStartEvent event) {
 					uploadStarted(new StartedEvent(upload, file.getFileName(), file.getType(), file.getFileSize()));
 				}
 
+				@Override
 				public void streamingFinished(StreamingEndEvent event) {
 					uploadFinished(null); // event doesnt matter here
+					uploadSucceeded(new SucceededEvent(upload, file.getFileName(), file.getType(), file.getFileSize()));
 				}
 
+				@Override
 				public void streamingFailed(StreamingErrorEvent event) {
 					uploadFailed(null);
 				}
 
+				@Override
 				public void onProgress(StreamingProgressEvent event) {
 					updateProgress(event.getBytesReceived(), event.getContentLength());
 				}
 
+				@Override
 				public boolean listenProgress() {
 					return true;
 				}
 
+				@Override
 				public boolean isInterrupted() {
 					return false;
 				}
 
+				@Override
 				public OutputStream getOutputStream() {
 					return receiveUpload(file.getFileName(), file.getType());
 				}
@@ -276,14 +320,14 @@ public class UploadComponent extends VerticalLayout implements ClickListener, Dr
 			upload.interruptUpload();
 	}
 
-	public void startedMode() {
-		upload.setVisible(true);
-		progressLayout.setVisible(false);
+	public void startMode() {
+		removeAllComponents();
+		addComponent(startLayout);
 	}
 
 	public void uploadMode() {
-		upload.setVisible(false);
-		progressLayout.setVisible(true);
+		removeAllComponents();
+		addComponent(uploadLayout);
 	}
 
 	public File getFile() {
